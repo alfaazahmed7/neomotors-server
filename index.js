c8265e66865adbdb1a88b13fd85f4129b36ca1a6
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-05-18 16:43:09 +0600

    feat: initialize Express server and connect to MongoDB

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const PORT = process.env.PORT;
+const uri = process.env.MONGODB_URI;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+});
